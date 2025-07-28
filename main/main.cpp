d56c707cc2021ee45c4b7c88ef9cfac2a0d5d4e6
braedenkloke <braedenkloke@gmail.com>
2025-07-28 08:18:46 +0000

    add stdout logger

--- main/main.cpp
+++ main/main.cpp
@@ -34,7 +34,8 @@ int main(int argc, char* argv[]) {
     auto model = std::make_shared<TopCoupled>("top", orders, cellAssemblyTime);
     auto rootCoordinator = cadmium::RootCoordinator(model);
 
-    rootCoordinator.setLogger<CSVLogger>(outputFile, ",");
+    rootCoordinator.setLogger<STDOUTLogger>(",");
+    //rootCoordinator.setLogger<CSVLogger>(outputFile, ",");
     rootCoordinator.start();
     rootCoordinator.simulate(maxSimulationTime);
     rootCoordinator.stop();	
